95fa62716c10800b025868ec24123a74332e5286
Dmitrii Elkin <d.elkin@sportxline.com>
2023-03-24 18:40:00 +0300

    ES modules + TS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { one } from '#utils'
 import { foo } from '#utils/foo.js'
-import { inner } from '#utils/inner'
+import { inner } from '#utils/inner/index.js'
 import { bar } from '#utils/inner/bar.js'
 
 one()
