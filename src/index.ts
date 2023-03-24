6b4d1f1e7c572732ed0a6939b65971afa739e75e
Dmitrii Elkin <d.elkin@sportxline.com>
2023-03-24 21:02:55 +0300

    Fix config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { one } from '#utils'
-import { foo } from '#utils/foo.js'
-import { inner } from '#utils/inner'
-import { bar } from '#utils/inner/bar.js'
+import { foo } from '#utils/foo.ts'
+import { inner } from '#utils/inner/index.ts'
+import { bar } from '#utils/inner/bar.ts'
 
 one()
 foo()
